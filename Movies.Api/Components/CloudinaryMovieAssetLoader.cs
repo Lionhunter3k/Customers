c7f6b937f346e8eba6a6d7ee2e91a7ea00ec2827
Cosmin Dodu <lionhunter@gmail.com>
2018-12-05 14:41:44 +0200

    added cloud cdn

---
+++ Movies.Api/Components/CloudinaryMovieAssetLoader.cs
@@ -0,0 +1,30 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Movies.Domain.Components;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Movies.Api.Components
+{
+    public class CloudinaryMovieAssetLoader : BaseMovieAssetLoader
+    {
+        private readonly Account _account;
+
+        public CloudinaryMovieAssetLoader(MovieAssetLoaderOptions movieAssetLoaderOptions, Account cloudinaryAccountOptions) : base(movieAssetLoaderOptions)
+        {
+            this._account = cloudinaryAccountOptions;
+        }
+
+        protected override async Task<string> InternalLoadAssetsAsync(Stream inputStream, Uri uri, string extension)
+        {
+            Cloudinary cloudinary = new Cloudinary(_account);
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(Guid.NewGuid() + extension, inputStream)
+            };
+            var uploadResult = await cloudinary.UploadAsync(uploadParams);
+            return uploadResult.Uri.ToString();
+        }
+    }
+}
